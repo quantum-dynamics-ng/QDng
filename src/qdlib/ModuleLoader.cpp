ff9dc421dd26d9c7d5528e8fc1e60738e4d72752
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2008-11-06 16:47:32 +0000

    Tryied to make the module loader compile :(

--- src/qdlib/ModuleLoader.cpp
+++ src/qdlib/ModuleLoader.cpp
@@ -1,15 +1,163 @@
+#include <dlfcn.h>
 #include "ModuleLoader.h"
+
 
 namespace QDLIB {
 
-ModuleLoader::ModuleLoader()
-{
+   ModuleLoader::ModuleLoader() : _user_path("~/qdmods/")
+   {
+   }
+   
+   
+   ModuleLoader::~ModuleLoader()
+   {
+      map<string, module>::iterator it;
+      
+      /* Unload the modules */
+      it = _mod_map.begin();
+      while (it != _mod_map.end() ){
+	 dlclose( it->second.handle );
+	 it++;
+      }
+   }
+   
+   bool QDLIB::ModuleLoader::_isLoaded( )
+   {
+      
+   }
+
+   
+   /**
+    * Register a WF module.
+    */
+   void ModuleLoader::_RegisterWF(void *handle, string &name)
+   {
+      typedef void (*InstanceWF_t)(void);
+      
+      InstanceWF_t InstanceWF = (InstanceWF_t) dlsym(handle, "InstanceWF");
+
+      if (InstanceWF != NULL){
+	 _mod_map[name].InstanceWF = InstanceWF;
+	 _mod_map[name].InstanceOP = NULL;
+	 _mod_map[name].link_count++;
+	 _mod_map[name].handle = handle;
+      } else {
+	 throw ( Exception("Invalid WaveFunction module") );
+      }
+   }
+   
+   /**
+    * Register an operator module.
+    */
+   void ModuleLoader::_RegisterOP(void *handle, string &name)
+   {
+      Operator* (*InstanceOP)(void) = NULL;
+      
+      InstanceWF = dlsym(handle, "InstanceWF");
+      if (InstanceWF != NULL){
+	 _mod_map[name].InstanceWF = NULL;
+	 _mod_map[name].InstanceOP = InstanceOP;
+	 _mod_map[name].link_count++;
+	 _mod_map[name].handle = handle;
+      } else {
+	 throw ( Exception("Invalid Operator module") );
+      }
+   }
+   
+   /**
+    * Set the user-defined module search path.
+    * 
+    * Only one path is allowed.
+    */
+   void ModuleLoader::UserPath( string &path )
+   {
+      _user_path = path;
+      if ( path[path.size()-1] != '/')
+	 _user_path += '/';
+     
+   }
+
+   
+   /**
+    * Load a WaveFunction Module.
+    * 
+    * If the the module is non-existend or provides the wrong interface,
+    * an exception will be thrown.
+    * 
+    * \return A fresh, empty instance of the wf class.
+    */
+   WaveFunction * ModuleLoader::LoadWF( string &name )
+   {
+      void* handle;
+      string s;
+      
+      
+      /* is already loaded? */
+      if (_isLoaded()) {
+	 _mod_map[name].link_count++;
+	 return _mod_map[name].InstanceWF();
+      }
+	 
+      /* try user path */
+      s = _user_path + name;
+      handle = dlopen(s.c_str(), RTLD_NOW );
+      if ( handle != NULL )
+      {
+	 _RegisterWF(handle, name);
+	 return _mod_map[name].InstanceWF;
+      }
+      
+      /* try system path */
+      handle = dlopen(name.c_str(), RTLD_NOW );
+      if ( handle != NULL )
+      {
+	 _RegisterWF(handle, name);
+	 return _mod_map[name].InstanceWF;
+      } else {
+	 throw ( Exception("No module found") );
+      }
+      
+   }
+   
+   /**
+    * Load a Operator Module.
+    * 
+    * If the the module is non-existend or provides the wrong interface,
+    * an exception will be thrown.
+    * 
+    * \return A fresh, empty instance of the operator class.
+    */
+   Operator * ModuleLoader::LoadOp( string & name )
+   {
+      /* is already loaded? */
+      if (_isLoaded()) {
+	 _mod_map[name].link_count++;
+	 return _mod_map[name].InstanceOP();
+      }
+	 
+      /* try user path */
+      s = _user_path + name;
+      handle = dlopen(s.c_str(), RTLD_NOW );
+      if ( handle != NULL )
+      {
+	 _RegisterOP(handle, name);
+	 return _mod_map[name].InstanceOP;
+      }
+      
+      /* try system path */
+      handle = dlopen(name.c_str(), RTLD_NOW );
+      if ( handle != NULL )
+      {
+	 _RegisterOP(handle, name);
+	 return _mod_map[name].InstanceOP;
+      } else {
+	 throw ( Exception("No module found") );
+      }
+   }
+
+
+
 }
 
 
-ModuleLoader::~ModuleLoader()
-{
-}
 
-
-}
--- src/qdlib/ModuleLoader.h
+++ src/qdlib/ModuleLoader.h
@@ -1,18 +1,51 @@
 #ifndef QDLIBMODULELOADER_H
 #define QDLIBMODULELOADER_H
 
+#include "WaveFunction.h"
+#include "Operator.h"
+
+#define MOD_BASENAME_WF "QD-WF-"
+#define MOD_BASENAME_OP "QD-OP-"
+
 namespace QDLIB {
 
-/**
-	@author Markus Kowalewski
-*/
-class ModuleLoader{
-public:
-    ModuleLoader();
-
-    ~ModuleLoader();
-
-};
+   /**
+    * Loader for dynamic linked objects.
+    * 
+    * @author Markus Kowalewski
+    */
+   class ModuleLoader
+   {
+      private:
+	 struct module {
+	    void *handle;
+	    int link_count;
+	    WaveFunction* (*InstanceWF)(void);
+	    Operator* (*InstanceOP)(void);
+	 };
+	 
+	 string _user_path;
+	 map<string, module> _mod_map;
+	 
+	 bool _isLoaded();
+	 void _RegisterWF(void *handle, string &name);
+	 void _RegisterOP(void *handle, string &name);
+	 
+	 class mod {
+	    public:
+	    static WaveFunction* InstanceWF();
+	 };
+      public:
+	 ModuleLoader();
+      
+	 ~ModuleLoader();
+	 
+	 
+	 void UserPath(string &path);
+	 WaveFunction* LoadWF(string &name);
+	 Operator* LoadOp(string &name);
+      
+   };
 
 }
 
