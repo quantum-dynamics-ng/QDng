721c1cdbf0cb99f31d3cd12003c2fd4edf484a21
Markus Kowalewski <mko@argos.cup.uni-muenchen.de>
2009-03-27 13:23:57 +0100

    Moved Bessel funcs to linalg. Changed OKspace into DSpace
    The D(iagonal)Space is a more general name/concept.

---
+++ src/linalg/Bessel.h
@@ -0,0 +1,22 @@
+#ifndef BESSEL_H
+#define BESSEL_H
+
+#include "math/typdefs.h"
+
+namespace QDLIB {
+
+
+   extern int BesselJ0 (int n, double arg, dVec &coeffs, int &zeroes);
+   extern int BesselI0 (int n, double arg, dVec &coeffs, int &zeroes);
+      
+   /* Slatec functions */
+   extern "C" { 
+      void zbesj_(double *re, double *im, double *alpha, int *kode, int *n,
+		  double *cRe, double *cIm, int *nz, int *ierr);
+      void zbesi_(double *re, double *im, double *alpha, int *kode, int *n,
+		  double *cRe, double *cIm, int *nz, int *ierr);
+   }
+
+}
+
+#endif
