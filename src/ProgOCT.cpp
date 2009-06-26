74fea7c6e681f7bb842e4cadcc566afc6a47ff5c
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2009-06-26 17:51:32 +0200

    Progress on OCT Programm

--- src/ProgOCT.cpp
+++ src/ProgOCT.cpp
@@ -2,18 +2,27 @@
 
 #include "tools/FileSingleDefs.h"
 #include "tools/Logger.h"
+#include "ChainLoader.h"
 
 namespace QDLIB {
 
    ProgOCT::ProgOCT(XmlNode &OCTNode) : _octNode(OCTNode), _ContentNodes(NULL),
-   _fname(DEFAULT_BASENAME_LASER), _dir(""),
+   _fname(DEFAULT_BASENAME_LASER), _dir(""), _iterations(DEFAULT_ITERATIONS), _convergence(DEFAULT_CONVERGENCE),
    _method(krotov), _coupling(dipole), _ttype(ov), _phase(false),  _ntargets(1), _alpha(1)
    {
+      for(int i=0; i < MAX_TARGETS; i++){
+	 PsiI[i] = NULL;
+	 PsiT[i] = NULL;
+      }
    }
    
    
    ProgOCT::~ProgOCT()
    {
+      for(int i=0; i < _ntargets; i++){
+	 if (PsiI[i] != NULL) delete PsiI[i];
+	 if (PsiT[i] != NULL) delete PsiT[i];
+      }
    }
 
    /**
@@ -66,6 +75,34 @@ namespace QDLIB {
 
       }
       
+      /* Read the shape file */
+      if (attr.isPresent("shape")){
+	 string shape;
+	 attr.GetValue("shape", shape);
+	 Laser::FileLaser file = _shape[0].File();
+	 
+	 file.Suffix(BINARY_O_SUFFIX);
+	 file.Name(shape);
+	 file >> &(_shape[0]);
+      } else
+	 throw (EParamProblem("No shape file given"));
+      
+	 
+      /* Number of iterations */
+      if (attr.isPresent("iterations")){
+	 attr.GetValue("iterations", _iterations);
+	 if (_iterations < 1)
+	    throw(EParamProblem("Invalid number of iterations", _iterations));
+      }
+      
+      /* Number of iterations */
+      if (attr.isPresent("conv")){
+	 attr.GetValue("conv", _convergence);
+	 if (_iterations < 0)
+	    throw(EParamProblem("Invalid convergency criteria", _convergence));
+      }
+
+      
       /* Type of coupling to use */
       if (attr.isPresent("coup")){
 	 attr.GetValue("coup", s);
@@ -94,6 +131,8 @@ namespace QDLIB {
 	 attr.GetValue("ntargets", _ntargets);
 	 if (_ntargets < 1)
 	    throw (EParamProblem("Number of targets to small", _ntargets));
+	 if (_ntargets > MAX_TARGETS)
+	    throw (EParamProblem("Number of targets exceeding MAX_TARGETS", _ntargets));	 
       }
 
       if (attr.isPresent("alpha")){
@@ -149,14 +188,205 @@ namespace QDLIB {
 	    log.cout() << "Optimize laserfield for dipole coupling" << endl;
       }
       
+      log.cout() << "Maximum number of iterations : " << _iterations << endl;
+      log.cout().precision(1);
+      log.cout() << "Convergence : " << scientific << _convergence << endl;
+      
       log.flush();
    }
   
+   
+
+   /**
+    * Calculate Laserfield for the next time step.
+    */
+   double ProgOCT::CalcLaserField( WaveFunction** wfi, WaveFunction** wft )
+   {
+      QDClock *clock = QDGlobalClock::Instance();  /* use the global clock */
+      
+      double res=0;
+      dcomplex im(0,0);
+      WaveFunction *opwf;
+      
+      
+      
+      opwf = wfi[0]->NewInstance();
+      
+      for (int t=0; t < _ntargets; t++){
+	 
+	 /* Imaginary term */
+	 switch(_coupling){
+	    case dipole:
+               dynamic_cast<OGridPotential*>(_Coup)->Apply( opwf, wfi[t]);
+	       break;
+	 }
+	 
+	 /* Phase sensitive */
+	 if (_phase)
+	    im = *wft[t] * opwf;
+	 else
+	    im = (*wfi[t] * wft[t] ) * (*wft[t] * opwf );
+		 
+	 res += im.imag();
+      }
+      
+      /* Method */
+      switch(_method){
+	 case krotov:
+		  res = _laser[0][clock->TimeStep()] -
+			_shape[0][clock->TimeStep()] / (_alpha * double(_ntargets)) * res;
+	    break;
+	 case rabitz:
+		  res = _shape[0][clock->TimeStep()] / (_alpha * double(_ntargets)) * res;
+	    break;
+      }
+      
+      return res;
+   }
+   
    /**
     * Run the OCT-Programm
     */
    void ProgOCT::Run()
    {
+      Logger& log = Logger::InstanceRef();
+      
+      XmlNode *section;
+      Operator *_h;
+      
       _InitParams( );
+      
+      /* fetch the ChildNodes */
+      _ContentNodes = _octNode.NextChild();
+      
+      log.Header( "QM Initialization", Logger::Section);
+      log.Header( "Propagator: ", Logger::SubSection);
+      
+      
+      /* Load & Init the propagator */
+      section = _ContentNodes->FindNode( "propagator" );
+      if (section == NULL)
+	 throw ( EParamProblem ("No propagator found") );
+      
+      _U = ChainLoader::LoadPropagator( section, &_h );
+      delete section;
+      
+      
+      /* Load Initial Wavefunctions */
+      log.Header( "Initial wave functions", Logger::SubSection);
+      char num[3];
+      for (int i=0; i< _ntargets; i++){
+	 snprintf(num, 3, "%d", i);
+	 section = _ContentNodes->FindNode( "wfi"+string(num) );
+	 if (section == NULL)
+	    throw ( EParamProblem ("Missing initial wavefunction", i) );
+	 PsiI[i] = ChainLoader::LoadWaveFunctionChain( section );
+	 delete section;
+      }
+      log.cout() << endl;
+      
+      /* Load Target Wavefunctions */
+      log.Header( "Target wave functions", Logger::SubSection);
+      for (int i=0; i< _ntargets; i++){
+	 snprintf(num, 3, "%d", i);
+	 section = _ContentNodes->FindNode( "wft"+string(num) );
+	 if (section == NULL)
+	    throw ( EParamProblem ("Missing target wavefunction", i) );
+	 PsiT[i] = ChainLoader::LoadWaveFunctionChain( section );
+	 delete section;
+      }
+      
+      log.cout() << endl;
+      
+      QDClock *clock = QDGlobalClock::Instance();  /* use the global clock */
+           
+      /* Make sure our hamiltonian is initalized */
+      _h->Clock( clock );
+      _h->Init(PsiI[0]);
+      log.coutdbg() << "Initial engergy: " << _h->Expec(PsiI[0]) << endl;
+      
+      /* Copy, since the propagator will propably scale it/modify etc. */
+      _H = _h->NewInstance();
+      *_H = _h; 
+      _H->Clock( clock );
+            
+      /* Let the Propagator do it's initalisation */
+      _U->Clock( clock );
+      _H->UpdateTime();
+      _U->Init(PsiI[0]);
+
+      /* Check & reference the coupling operator */
+      bool coupling_ok = false;
+      switch(_coupling){
+	 case dipole:
+	    OGridDipole *CoupOGridDipole;
+	    CoupOGridDipole = dynamic_cast<OGridDipole*>(FindOperatorType<OGridDipole>(_h));
+	    if (CoupOGridDipole != NULL){
+	       _laser[0] = CoupOGridDipole->GetLaser();
+	       _Coup = (Operator*) CoupOGridDipole;
+	       coupling_ok=true;
+	    }
+	    
+	    break;
+      }
+      
+      if (!coupling_ok)
+	 throw ( EParamProblem ("No suitable coupling operator found in hamiltonian") );
+      
+      /* Objects for propagation */
+      WaveFunction* phii[MAX_TARGETS];
+      WaveFunction* phit[MAX_TARGETS];
+      
+      for (int i=0; i < _ntargets; i++){
+	 phii[i] = PsiI[i]->NewInstance();
+	 phit[i] = PsiT[i]->NewInstance();
+      }
+
+      
+      /* Iteration loop */
+      int i=0;
+      double deltaTarget=1;
+            
+      while (i < _iterations && deltaTarget > _convergence){
+	 /* Init with fresh wfs */
+	 for (int t=0; t < _ntargets; t++){
+	    *(phii[t]) = PsiI[t];
+	    *(phit[t]) = PsiT[t];
+	 }
+
+	 /* Backpropagation of targets */
+	 /** \todo prepare operator for fast forward/backward switching */
+	 clock->End();
+	 for (int s=0; s < clock->Steps(); s++){
+	    for (int t=0; t < _ntargets; t++){
+	       _U->Apply(phit[t]);
+	    }
+	    *(clock)--;
+	 }
+
+	 /* Forward Propagation */
+	 clock->Begin();
+	 for (int s=0; s < clock->Steps(); s++){
+	    _laser[0][clock->TimeStep()] = CalcLaserField(phii,phit);
+	    for (int t=0; t < _ntargets; t++){
+	       _U->Apply(phit[t]);
+	       _U->Apply(phii[t]);
+	    }
+	    *(clock)++;
+	 }
+	 
+	 /* Write Laserfield */
+	 i++; 
+      }
+
+      /* Remove tmp WFs */
+      for (int i=0; i < _ntargets; i++){
+	 delete phii[i];
+	 delete phit[i];
+      }
+
    }
-}
+
+
+
+} /* namespace QDLIB */
--- src/ProgOCT.h
+++ src/ProgOCT.h
@@ -5,9 +5,13 @@
 #include "tools/XmlNode.h"
 #include "tools/QDGlobalClock.h"
 #include "qdlib/OPropagator.h"
-
+#include "qdlib/OGridDipole.h"
 
 #define DEFAULT_BASENAME_LASER "laser"
+#define DEFAULT_ITERATIONS 50
+#define DEFAULT_CONVERGENCE 1e-6
+#define MAX_TARGETS 16
+#define MAX_LASERS 4
 
 namespace QDLIB {
 
@@ -27,6 +31,9 @@ namespace QDLIB {
    * \li phase    phase sensitive objective (true|false, default false)
    * \li ntargets Number of targets
    * \li alpha    The Field restriction value
+   * \li iterations Maximum number of iterations
+   * \li conv     Target convergence. Stop iterating if below convergence
+   * \li shape    Name of shapefile
    * 
    * @author Markus Kowalewski <markus.kowalewski@cup.uni-muenchen.de>
    */
@@ -42,6 +49,9 @@ namespace QDLIB {
 	 string _fname;
 	 string _dir;
 	 
+	 int _iterations;
+	 double _convergence;
+	 
 	 _method_t _method;
 	 _coupling_t _coupling;
 	 _ttype_t _ttype;
@@ -50,10 +60,20 @@ namespace QDLIB {
 	 int _ntargets;
 	 double _alpha;
 	 
+	 OPropagator *_U;
+	 Operator *_H;
+	 Operator *_Coup;
+	 
+	 Laser _laser[MAX_LASERS];
+	 Laser _shape[MAX_LASERS];
+	 
+	 WaveFunction* PsiI[MAX_TARGETS];
+	 WaveFunction* PsiT[MAX_TARGETS];
+	 
 	 void _InitParams();
 	 
       protected:
-	 void CalcLaserField();
+	 double CalcLaserField(WaveFunction** wfi, WaveFunction** wft);
 	 
       public:
 	 ProgOCT(XmlNode &OCTNode);
