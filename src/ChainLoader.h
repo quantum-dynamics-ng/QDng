667ea2392ccd80aade00c527fc84bd0b5f7eca21
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2009-06-26 13:37:13 +0200

    Added Search funtion for searching specific classed  in operator collections.

--- src/ChainLoader.h
+++ src/ChainLoader.h
@@ -3,7 +3,8 @@
 
 #include "qdlib/OPropagator.h"
 #include "tools/XmlNode.h"
-
+#include "qdlib/OSum.h"
+#include "qdlib/OMultistate.h"
 
 namespace QDLIB
 {
@@ -21,6 +22,45 @@ namespace QDLIB
 	 static OPropagator* LoadPropagator( XmlNode *Unode, Operator **Hamiltonian);
    };
 
-}
+   /**
+    * Find a specific type of operator in Chain of Sum/Multistate operators.
+    */
+   template <class C>
+   Operator* FindOperatorType(Operator *O)
+   {
+      C* t;
+      Operator* res = NULL;
+	    
+      t = dynamic_cast<C*>(O);
+      if (t != NULL) return t;
+      
+      /* Crawl trough Sum operator*/
+      OSum* sum;
+      sum = dynamic_cast<OSum*>(O);
+      if (sum != NULL){
+	 for(int i=0; i < sum->Size(); i++){
+	     res = FindOperatorType<C>((*sum)[i]);
+	     if (res != NULL) return res;
+	 }
+      }
+      
+      /* Crawl trough Multistate operator*/
+      OMultistate* ms;
+      ms = dynamic_cast<OMultistate*>(O);
+      if (ms != NULL){
+	 for(int i=0; i < ms->States(); i++){
+	    for(int j=0; j < ms->States(); j++){
+	       if (ms->State(i,j) != NULL){
+		  res = FindOperatorType<C>(ms->State(i,j));
+		  if (res != NULL) return res;
+	       }
+	    }
+	 }
+      }
+      
+      return NULL;
+   }
+
+} /* namespace QDLIB */
 
 #endif
