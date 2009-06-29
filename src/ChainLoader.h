7f73a63b0d441fd6a04f13ddeff9b1e10ecb0762
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2009-06-29 18:41:05 +0200

    Changed Operator Finder

--- src/ChainLoader.h
+++ src/ChainLoader.h
@@ -24,23 +24,35 @@ namespace QDLIB
 
    /**
     * Find a specific type of operator in Chain of Sum/Multistate operators.
+    * 
+    * \param O       The Operator to inspect.
+    * \param found   If type is found the naked operator will be placed here
+    * 
+    * \return The whole Operator with packaging (single state or multistate)
     */
    template <class C>
-   Operator* FindOperatorType(Operator *O)
+   Operator* FindOperatorType(Operator *O, C **found)
    {
       C* t;
       Operator* res = NULL;
-	    
+      C *sop=NULL;
+      
       t = dynamic_cast<C*>(O);
-      if (t != NULL) return t;
+      if (t != NULL){
+	 *found = t;
+	 return t;
+      }
       
       /* Crawl trough Sum operator*/
       OSum* sum;
       sum = dynamic_cast<OSum*>(O);
       if (sum != NULL){
 	 for(int i=0; i < sum->Size(); i++){
-	     res = FindOperatorType<C>((*sum)[i]);
-	     if (res != NULL) return res;
+	     res = FindOperatorType<C>((*sum)[i], &sop);
+	     if (res != NULL){
+		*found = sop;
+		return res;
+	     }
 	 }
       }
       
@@ -48,14 +60,21 @@ namespace QDLIB
       OMultistate* ms;
       ms = dynamic_cast<OMultistate*>(O);
       if (ms != NULL){
+	 OMultistate* package = new OMultistate();
+	 package->States(ms->States());
 	 for(int i=0; i < ms->States(); i++){
 	    for(int j=0; j < ms->States(); j++){
 	       if (ms->State(i,j) != NULL){
-		  res = FindOperatorType<C>(ms->State(i,j));
-		  if (res != NULL) return res;
+		  res = FindOperatorType<C>(ms->State(i,j), &sop);
+		  if (res != NULL){
+		     package->Add(res, i, j);
+		     *found = sop;
+		     return package;
+		  }
 	       }
 	    }
 	 }
+	 delete package;
       }
       
       return NULL;
