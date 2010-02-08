1dc97ac5dbadb05eb0d3be930d98da55427fab03
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2010-02-08 23:13:47 +0100

    Cleaned up InternalMods: Introduced macros

--- src/qdlib/InternalMods.cpp
+++ src/qdlib/InternalMods.cpp
@@ -1,9 +1,9 @@
 #include "InternalMods.h"
 
-
 /* Operator list */
 #include "OCheby.h"
 #include "OSPO.h"
+#include "OGSPO.h"
 #include "OSIL.h"
 
 #include "OGridNablaSq.h"
@@ -24,76 +24,46 @@
 #include "WFGridCartesian.h"
 #include "WFLevel.h"
 
+/**
+ * Macro for creating functions which generates an instance of the class.
+ * */
+#define QDNG_OPERATOR_INSTANCE_FUNCTION(CLASSNAME) \
+   Operator* INT_##CLASSNAME() \
+   { \
+      CLASSNAME *p; \
+      p = new CLASSNAME (); \
+      return p; \
+   }
+
+#define QDNG_WAVEFUNCTION_INSTANCE_FUNCTION(CLASSNAME) \
+   WaveFunction* INT_##CLASSNAME() \
+   { \
+      CLASSNAME* p; \
+      p = new CLASSNAME(); \
+      return p; \
+   }
+
+#define QDNG_STRING_TO_INSTANCE_PTR(CLASSNAME) \
+   if (name == "INT_" #CLASSNAME) return &INT_##CLASSNAME;
 
 namespace QDLIB
 {
 
-
    /* Instance functions  */
-   Operator* INT_OCheby()
-   {
-      OCheby *p;
-      p = new OCheby ();
-      return p;
-   }
 
-   Operator* INT_OSPO()
-   {
-      OSPO *p;
-      p = new OSPO ();
-      return p;
-   }
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OCheby)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGSPO)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OSPO)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OSIL)
 
-   Operator* INT_OSIL()
-   {
-      OSIL *p;
-      p = new OSIL ();
-      return p;
-   }
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridNablaSq)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridGMat)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridPotential)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridDipole)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridPosition)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridNabla)
 
-
-   Operator* INT_OGridNablaSq()
-   {
-      OGridNablaSq *p;
-      p = new OGridNablaSq ();
-      return p;
-   }
-
-   Operator* INT_OGridGMat()
-   {
-      OGridGMat *p;
-      p = new OGridGMat ();
-      return p;
-   }
-
-   Operator* INT_OGridPotential()
-   {
-      OGridPotential *p;
-      p = new OGridPotential ();
-      return p;
-   }
-
-   Operator* INT_OGridDipole()
-   {
-      OGridDipole *p;
-      p = new OGridDipole ();
-      return p;
-   }
-
-   Operator* INT_OGridPosition()
-   {
-      OGridPosition *p;
-      p = new OGridPosition();
-      return p;
-   }
-
-   Operator* INT_OGridNabla()
-   {
-      OGridNabla *p;
-      p = new OGridNabla();
-      return p;
-   }
-
+   /* Leave this here because we have an individual Constructor */
    Operator* INT_Alias_OGridMomentum()
    {
       OGridNabla *p;
@@ -101,94 +71,50 @@ namespace QDLIB
       return p;
    }
 
-   Operator* INT_OGridNAC()
-   {
-      OGridNAC *p;
-      p = new OGridNAC();
-      return p;
-   }
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGridNAC)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OGobbler)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OProjection)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OScalar)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OHermitianMatrix)
+   QDNG_OPERATOR_INSTANCE_FUNCTION(OMatDipole)
 
-   Operator* INT_OGobbler()
-   {
-      OGobbler *p;
-      p = new OGobbler ();
-      return p;
-   }
-
-   Operator* INT_OProjection()
-   {
-      OProjection *p;
-      p = new OProjection ();
-      return p;
-   }
-
-   Operator* INT_OScalar()
-   {
-      OScalar *p;
-      p = new OScalar ();
-      return p;
-   }
-
-   Operator* INT_OHermitianMatrix()
-   {
-      OHermitianMatrix *p;
-      p = new OHermitianMatrix ();
-      return p;
-   }
-
-   Operator* INT_OMatDipole()
-   {
-      OMatDipole *p;
-      p = new OMatDipole ();
-      return p;
-   }
-
-
-   WaveFunction* INT_WFGridCartesian()
-   {
-      WFGridCartesian* p;
-      p = new WFGridCartesian();
-      return p;
-   }
-
-
-   WaveFunction* INT_WFLevel()
-   {
-      WFLevel* p;
-      p = new WFLevel();
-      return p;
-}
+   QDNG_WAVEFUNCTION_INSTANCE_FUNCTION(WFGridCartesian)
+   QDNG_WAVEFUNCTION_INSTANCE_FUNCTION(WFLevel)
 
    instWF* InternalWF(const string &name)
    {
-      if (name == "INT_WFGridCartesian") return &INT_WFGridCartesian;
-      if (name == "INT_WFLevel") return &INT_WFLevel;
+      QDNG_STRING_TO_INSTANCE_PTR(WFGridCartesian)
+      QDNG_STRING_TO_INSTANCE_PTR(WFLevel)
+
       return NULL;
    }
 
    instOP* InternalOP(const string &name)
    {
-      if (name == "INT_OCheby") return &INT_OCheby;
-      if (name == "INT_OSPO") return &INT_OSPO;
-      if (name == "INT_OSIL") return &INT_OSIL;
+      QDNG_STRING_TO_INSTANCE_PTR(OCheby)
+      QDNG_STRING_TO_INSTANCE_PTR(OSPO)
+      QDNG_STRING_TO_INSTANCE_PTR(OGSPO)
+      QDNG_STRING_TO_INSTANCE_PTR(OSIL)
 
-      if (name == "INT_OGridNablaSq") return &INT_OGridNablaSq;
-      if (name == "INT_OGridGmat") return &INT_OGridGMat;
-      if (name == "INT_OGridPotential") return &INT_OGridPotential;
-      if (name == "INT_OGridDipole") return &INT_OGridDipole;
-      if (name == "INT_OGridPosition") return &INT_OGridPosition;
+      QDNG_STRING_TO_INSTANCE_PTR(OGridNablaSq)
+      QDNG_STRING_TO_INSTANCE_PTR(OGridNablaSq)
+      QDNG_STRING_TO_INSTANCE_PTR(OGridPotential)
+      QDNG_STRING_TO_INSTANCE_PTR(OGridDipole)
+      QDNG_STRING_TO_INSTANCE_PTR(OGridPosition)
+
+      /* This is an alias */
       if (name == "INT_OGridMomentum") return &INT_Alias_OGridMomentum;
-      if (name == "INT_OGridNabla") return &INT_OGridNabla;
-      if (name == "INT_OGridNAC") return &INT_OGridNAC;
 
-      if (name == "INT_OHermitianMatrix") return &INT_OHermitianMatrix;
-      if (name == "INT_OMatDipole") return &INT_OMatDipole;
-      if (name == "INT_OGobbler") return &INT_OGobbler;
-      if (name == "INT_OProjection") return &INT_OProjection;
-      if (name == "INT_OScalar") return &INT_OScalar;
+      QDNG_STRING_TO_INSTANCE_PTR(OGridNabla)
+      QDNG_STRING_TO_INSTANCE_PTR(OGridNAC)
+
+      QDNG_STRING_TO_INSTANCE_PTR(OHermitianMatrix)
+      QDNG_STRING_TO_INSTANCE_PTR(OMatDipole)
+      QDNG_STRING_TO_INSTANCE_PTR(OGobbler)
+      QDNG_STRING_TO_INSTANCE_PTR(OProjection)
+      QDNG_STRING_TO_INSTANCE_PTR(OScalar)
 
       return NULL;
    }
-
 
 }
