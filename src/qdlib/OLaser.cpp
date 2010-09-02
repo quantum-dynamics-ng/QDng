af2a2a9698bca8e7f5ace958259cafbe0120df95
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2010-09-02 15:26:13 +0200

    Added abstract Laser Operator class to unify access to laser of Dipole Operators etc.

---
+++ src/qdlib/OLaser.cpp
@@ -0,0 +1,86 @@
+#include "OLaser.h"
+
+namespace QDLIB {
+
+   OLaser::OLaser() : _numlasers(1), _Pol(NULL), _UsePol(false)
+   {
+      _laser = new Laser[1];
+   }
+   
+   QDLIB::OLaser::OLaser(int num) :  _numlasers(num), _Pol(NULL), _UsePol(false)
+   {
+      _laser = new Laser[num];
+   }
+   
+   OLaser::~OLaser()
+   {
+      delete[] _laser;
+      if (_Pol != NULL) delete[] _Pol;
+   }
+
+   /**
+    * This decides wether the to use polarisation vectors or not.
+    * 
+    * All former vectors will be destroyed.
+    */
+   void OLaser::UsePol(bool use)
+   {
+      _UsePol = use;
+      
+      if (use){
+	 if (_Pol != NULL) delete[] _Pol;
+	 _Pol = new dVec[_numlasers];
+      }
+   }
+
+
+   /**
+    * Set the number of laser fields.
+    * 
+    * All previously contained fields will be destroyed.
+    */
+   void OLaser::NumLaser(int num)
+   {
+      delete[] _laser;
+      if (_Pol != NULL) delete[] _Pol;
+      
+      _laser = new Laser[num];
+      if (_UsePol) _Pol = new dVec[num];
+      
+      _numlasers = num;
+   }
+
+   /**
+    * Set Laser (index=0).
+    */
+   void OLaser::SetLaser(Laser & laser)
+   {
+      SetLaser(0, laser);
+   }
+
+   /**
+    * Set Laser with index n.
+    */
+   void OLaser::SetLaser(int n, Laser & laser)
+   {
+      _laser[n] = laser;
+   }
+   
+   
+   void OLaser::SetPol(dVec & pol)
+   {
+      SetPol(0, pol);
+   }
+
+   void OLaser::SetPol(int n, dVec & pol)
+   {
+      _Pol[n] = pol;
+   }
+   
+}
+
+
+
+
+
+
---
+++ src/qdlib/OLaser.h
@@ -0,0 +1,55 @@
+#ifndef QDLIBOLASER_H
+#define QDLIBOLASER_H
+
+#include "qdlib/Operator.h"
+#include "qdlib/Laser.h"
+
+namespace QDLIB {
+
+   /**
+    * Abstract base class for all operator types using an arbitrary number of laser fields.
+    *
+    * This class serves as a container for laser fields as well as a uniform interface to access
+    * the laser for external needs (OCT e.g.)
+    * 
+    * @author Markus Kowalewski <markus.kowalewski@cup.uni-muenchen.de>
+    */
+   class OLaser: public Operator
+   {
+      private:
+	 int _numlasers;
+	 Laser* _laser;        /* Laserpointer :) */
+	 dVec* _Pol;           /* Polarisation vectors */
+	 bool _UsePol;         /* Use the polarisation vecotors ? */
+	 
+      protected:
+	 void UsePol(bool use);
+	 void NumLaser(int num);
+	 
+      public:
+	 OLaser();
+	 OLaser(int num);
+	 
+	 ~OLaser();
+	 
+	 void SetLaser(Laser &laser);
+	 void SetLaser(int n, Laser &laser);
+	 
+	 Laser* GetLaser() { return &(_laser[0]); } 
+	 Laser* GetLaser(int n) { return &(_laser[n]); } 
+	 
+	 /* Return the number of lasers in use */
+	 int NumLasers() { return _numlasers; }
+   
+	 bool UsePol() { return _UsePol; }
+	 
+	 dVec& GetPol() { return _Pol[0]; } 
+	 dVec& GetPol(int n) { return _Pol[n]; } 
+	 
+	 void SetPol(dVec &pol);
+	 void SetPol(int n, dVec &pol);
+   };
+
+}
+
+#endif
