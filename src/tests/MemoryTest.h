b92d15322ccb35c4da991cddeccb475242a09056
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2011-10-23 17:27:26 +0200

    Add memory manager

---
+++ src/tests/MemoryTest.h
@@ -0,0 +1,29 @@
+/*
+ * MemoryTest.h
+ *
+ *  Created on: 23.10.2011
+ *      Author: markus
+ */
+
+#ifndef MEMORYTEST_H_
+#define MEMORYTEST_H_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class MemoryTest : public CppUnit::TestFixture
+{
+   CPPUNIT_TEST_SUITE( MemoryTest );
+   CPPUNIT_TEST( CORE_Test );
+   CPPUNIT_TEST( API_Test );
+   CPPUNIT_TEST_SUITE_END();
+   public:
+      MemoryTest();
+      ~MemoryTest();
+
+   protected:
+      void CORE_Test();
+      void API_Test();
+};
+
+#endif /* MEMORYTEST_H_ */
