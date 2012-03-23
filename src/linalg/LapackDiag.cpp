5a262418b85f3a6cec68eb0f60274decca2cd687
Markus Kowalewski <mkopc@cup.uni-muenchen.de>
2012-03-23 14:11:10 +0100

    Removed workspace handling from LAPACK part.
    feature is replaced by Memory class.

--- src/linalg/LapackDiag.cpp
+++ src/linalg/LapackDiag.cpp
@@ -1,103 +1,11 @@
 #include "LapackDiag.h"
 #include <stdlib.h>
 
-#include "tools/Collector.h"
+#include "tools/Memory.h"
 
 using namespace QDLIB;
 
 namespace LAPACK {
-   
-   
-   
-   
-   /**
-    * Manage workspace memory with singleton.
-    */
-   class WsList {
-      private:
-	 static WsList *ref;
-	 
-	 double*  dbuf[LAPACK_MAX_WSLIST];
-	 dcomplex* cbuf[LAPACK_MAX_WSLIST];
-	 size_t sized[LAPACK_MAX_WSLIST];
-	 size_t sizec[LAPACK_MAX_WSLIST];
-	 
-	 WsList()
-	 {
-	    for (int i=0; i < LAPACK_MAX_WSLIST; i++){
-	       sized[i] = 0;
-	       sizec[i] = 0;
-               dbuf[i] = NULL;
-	    }
-	 }
-	 WsList(WsList &cp){}
-      public:
-	 ~WsList()
-	 {
-	 }
-	 
-	 static WsList* Instance()
-	 {
-	    if (ref == NULL) ref = new WsList();
-
-	    return ref;
-	 }
-	 
-	 double* Getdbuf(size_t size)
-	 {
-	    /* look for buff of same size or larger */
-	    int i=0;
-	    while (i < LAPACK_MAX_WSLIST){
-	       if (sized[i] >= size) return dbuf[i];
-	       i++;
-	    }
-	    /* resize smallest buffer */
-	    i=0;
-	    unsigned int min = 1;
-	    unsigned int min_idx = 0;
-	    while (i < LAPACK_MAX_WSLIST){
-	       if (sized[i] < min) {
-		  min_idx = i;
-		  min = sized[i];
-	       }
-               i++;
-	    }
-	    if (sized[min_idx] > 0) QDLIB::Collector<double>::Instance()->Delete(dbuf[min_idx]);
-	    i=posix_memalign((void**) &dbuf[min_idx], 2*sizeof(double), size*sizeof(double));
-	    QDLIB::Collector<double>::Instance()->Register(dbuf[min_idx], true);
-	    return dbuf[min_idx];
-	 }
-	 
-	 dcomplex* Getcbuf(size_t size)
-	 {
-	    /* look for buff of same size or larger */
-	    int i=0;
-	    while (i < LAPACK_MAX_WSLIST){
-	       if (sizec[i] >= size) return cbuf[i];
-	       i++;
-	    }
-	    /* resize smallest buffer */
-	    i=0;
-	    unsigned int min = 1;
-	    unsigned int min_idx = 0;
-	    while (i < LAPACK_MAX_WSLIST){
-	       if (sizec[i] < min) {
-		  min_idx = i;
-		  min = sizec[i];
-	       }
-               i++;
-	    }
-	    if (sizec[min_idx] > 0) delete cbuf[min_idx];
-	    i=posix_memalign((void**) &cbuf[min_idx], 2*sizeof(dcomplex), size*sizeof(dcomplex));
-	    return cbuf[min_idx];
-	 }
-    };
-
-    WsList* WsList::ref = 0;
-    
-//	 WsList_s<double> dWsList[LAPACK_MAX_WSLIST];
-	 //WsList_s<dcomplex> cWsList[LAPACK_MAX_WSLIST];
-   
    
    /**
     * Diagonalize a real symmetric matrix.
@@ -112,7 +20,6 @@ namespace LAPACK {
       int ws_size;
       int size;
       int info=0;
-      WsList *ws = WsList::Instance();
       
       if (mat->rows() != mat->cols()) return -1;
       if (evals->strides() != 1) return -2;
@@ -125,9 +32,13 @@ namespace LAPACK {
       char jobz = 'V';
       char uplo = 'U';
       
+      double *ws;
+      Memory::Instance().Align((void**) &ws, ws_size*sizeof(double));
       DSYEV_F77(&jobz, &uplo, &size, mat->begin(),  &size,
-		 evals->begin(0) , ws->Getdbuf(ws_size), &ws_size, &info);
+		 evals->begin(0) , ws, &ws_size, &info);
       
+      Memory::Instance().Free( (void*) ws );
+
       return info;
    }
    
@@ -136,7 +47,6 @@ namespace LAPACK {
       int ws_size;
       int size;
       int info;
-      WsList *ws = WsList::Instance();
       
       if (mat->rows() != mat->cols()) return -1;
       if (evals->strides() != 1) return -2;
@@ -149,9 +59,18 @@ namespace LAPACK {
       char jobz = 'V';
       char uplo = 'U';
       
+      double *ws;
+      Memory::Instance().Align( (void**) &ws, ws_size*sizeof(dcomplex));
+
+      dcomplex *wsC;
+      Memory::Instance().Align((void**) &wsC, ws_size*sizeof(double));
+
       ZHEEV_F77(&jobz, &uplo, &size, mat->begin(),  &size,
-	     evals->begin(0) , ws->Getcbuf(ws_size), &ws_size, ws->Getdbuf(ws_size), &info);
+	     evals->begin(0) , wsC , &ws_size, ws, &info);
       
+      Memory::Instance().Free( (void*) ws );
+      Memory::Instance().Free( (void*) wsC );
+
       return info;
    }
    
@@ -162,13 +81,17 @@ namespace LAPACK {
       int ws_size;
       int info;
       
-      WsList *ws = WsList::Instance();
       ws_size = 2*n-2;
       
       evec->newsize(n,n);
       
-      DSTEV_F77(&jobz, &n ,diag->begin(0), subdiag->begin(0), evec->begin(),  &n, ws->Getdbuf(ws_size), &info);
+      double *ws;
+      Memory::Instance().Align((void**) &ws, ws_size*sizeof(double));
+
+      DSTEV_F77(&jobz, &n ,diag->begin(0), subdiag->begin(0), evec->begin(),  &n, ws, &info);
       
+      Memory::Instance().Free( (void*) ws );
+
       return info;
    }
    
