30b38294ff95f0294556e5834e12924dc09b8b99
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2019-10-15 14:33:54 +0200

    [Multistate] increase number of state 16 -> 64

--- src/qdlib/WFMultistate.h
+++ src/qdlib/WFMultistate.h
@@ -3,14 +3,14 @@
 
 #include "qdlib/WaveFunction.h"
 
-#define QD_MAX_STATES 16
+#define QD_MAX_STATES 64
 
 namespace QDLIB
 {
 
    /**
     * Multistate wave function acts as a container for WaveFunction.
-    * 
+    *
     * @author Markus Kowalewski <markus.kowalewski@cup.uni-muenchen.de>
     */
    class WFMultistate : public WaveFunction
@@ -19,36 +19,36 @@ namespace QDLIB
 	 string _name;
 	 lint _nstates;
 	 WaveFunction* _states[QD_MAX_STATES];
-	 
+
 	 void _destroy();
       public:
          WFMultistate();
          ~WFMultistate();
 
 	 void Add(WaveFunction* Psi, lint index);
-	 
+
 	 lint States();
 	 WaveFunction* State(lint index);
-	 
+
 	 void Init(int reqstates);
 
 	 /* Interface implementation WaveFunction */
 	 virtual WaveFunction* NewInstance();
-	 
+
 	 virtual void Init(ParamContainer &params);
-	 
+
 	 virtual const string& Name();
-	 
+
 	 virtual double Norm();
-	 	 
+
 	 virtual void Normalize();
-      
+
 	 virtual WaveFunction* operator=(WaveFunction* Psi);
-	 
+
 	 virtual dcomplex operator*(WaveFunction* Psi);
-         
+
          virtual void IsKspace(bool is);
-         
+
          virtual WaveFunction* Reduce(double tolerance);
 
          virtual void Restore(WaveFunction* Psi);
