0457f32d9dc0a252478d82b455f4428a81d60b70
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2022-03-18 14:20:11 +0100

    OList: increase maximum number of operators to 128

--- src/qdlib/OList.h
+++ src/qdlib/OList.h
@@ -4,7 +4,7 @@
 #include "qdlib/Operator.h"
 #include <map>
 
-#define MAX_OPS 16
+#define MAX_OPS 128
 
 namespace QDLIB {
 
@@ -15,19 +15,19 @@ namespace QDLIB {
    {
       private:
          int _size;
-         
+
          Operator* _O[MAX_OPS];
          map<string,uint> _NamedEntries; /* holds name<->index mapping*/
       public:
          OList();
          ~OList();
-         
+
          Operator* operator[](int i);
-         
+
          Operator* operator[](const string& name);
 
          Operator* Get(int index);
-         
+
          Operator* Get(const string& name);
 
          int GetIndex(const string& name);
@@ -35,32 +35,32 @@ namespace QDLIB {
          bool Exists(const string& name);
 
          void Set(int index, Operator *O);
-         
+
          void Set(const string& name, Operator *O);
 
          int Size() const;
-         
+
          void Add(Operator *O);
-         
+
          void Add(const string& name, Operator *O);
 
          /* Overide Clock setter */
          virtual void Clock (QDClock *cl);
-         
+
          virtual void UpdateTime();
-         
+
          virtual void Conj(bool conj);
 
          virtual void Init(ParamContainer &params) {}
-         
+
          virtual void Init( WaveFunction * Psi );
-         
+
          virtual Operator* operator=(Operator* O);
-         
+
          virtual Operator* Copy(Operator* O);
-         
+
          virtual bool Valid(WaveFunction *Psi);
-         
+
          virtual void RecalcInternals(bool on);
    };
 
